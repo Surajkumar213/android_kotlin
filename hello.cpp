d51116a9b762e78280a1b7fd8eab99610700bbf0
Surajkumar213 <surajkumar098745@gmail.com>
2024-10-04 10:51:32 +0530

    pcommit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello Suraj!";
+    int n;
+    cout<<endl;
+    cout<<"Please Enter a number: ";
+    cin>>n;
+
+    cout<<"you just intered this number: "<<n;
+
+    return 0;
+}
